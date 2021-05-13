bf1be154fa6fcd2092339052fdbd88d1b099822d
bomin.kim <iambxmk@gmail.com>
2021-05-13 21:41:14 +0900

    Set router and api call

---
+++ src/views/AskView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div v-for="user in users">{{ user.title }}</div>
+  </div>
+</template>
+
+<script>
+import { fetchAskList } from '../api/index.js';
+
+export default {
+  data() {
+    return {
+      users: [],
+    }
+  },
+  created() {
+    fetchAskList()
+      .then(response => this.users = response.data)
+      .catch(error => console.log(error))
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/JobsView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div v-for="user in users">{{ user.domain }}</div>
+  </div>
+</template>
+
+<script>
+import { fetchJobsList } from '../api/index.js';
+
+export default {
+  data() {
+    return {
+      users: [],
+    }
+  },
+  created() {
+    fetchJobsList()
+      .then(response => this.users = response.data)
+      .catch(error => console.log(error))
+  }
+}
+</script>
+
+<style>
+
+</style>
